9a6b3dab34cf92c2fbe09dd3f69305f30cdc6b12
Felipe Oliveira <felipe.oliveira@rumosolucoes.com>
2023-09-14 11:59:38 -0300

    adicao styletcomponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import styled from "styled-components/native";
+
+const TextNew = styled.Text`
+  color: blue;
+  font-size: 24px;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>hello world</Text>
+      <Text style={styles.container}>hello world</Text>
+      <TextNew>Hello world</TextNew>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: "orange",
+  },
+});
 
 export default App;
