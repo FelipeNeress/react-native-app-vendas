b6016bb6bd98de41f4ffbdb24d6a04361fd35db5
Felipe Oliveira <felipe.oliveira@rumosolucoes.com>
2023-09-21 13:54:11 -0300

    primeiras etapas da criação do botão

--- src/modules/login/screens/login.tsx
+++ src/modules/login/screens/login.tsx
@@ -1,16 +1,15 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
+import Button from "../../../shared/components/button/button";
 import Input from "../../../shared/components/input/input";
 import { ContainerLogin } from "../styles/login.style";
-import Button from "../../../shared/components/button/button";
 
 const Login = () => {
-  
   return (
     <View>
       <ContainerLogin>
-        <Input placeholder="Usuario"/>
-        <Button margin="8px" title="ENTRAR"/>
+        <Input placeholder="Usuario" />
+        <Button margin="8px" title="ENTRAR" />
       </ContainerLogin>
     </View>
   );
