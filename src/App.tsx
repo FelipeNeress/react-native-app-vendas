8b09e7c7177760076b1b819a9408e9751a6eb4b0
Felipe Oliveira <felipe.oliveira@rumosolucoes.com>
2023-09-21 10:36:19 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
-import styled from "styled-components/native";
+import { SafeAreaView } from "react-native";
 
-import Teste from "./Teste";
-
-const TextNew = styled.Text`
-  color: blue;
-  font-size: 24px;
-`;
+import Login from "./modules/login";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <Teste text="ABC">Texxxtando vc</Teste>
-      <Teste text="Felipe" />
-      <Teste text="Gabriel" />
-      <Text style={styles.container}>hello world</Text>
-      <TextNew>Hello world</TextNew>
-    </SafeAreaView>
-  );
+  <SafeAreaView>
+    <Login />
+  </SafeAreaView>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    color: "orange",
-  },
-});
 
 export default App;
