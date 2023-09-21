0399d6bf67c5cf4f7ffdde4ee26a0389339d6d22
Felipe Oliveira <felipe.oliveira@rumosolucoes.com>
2023-09-21 11:40:23 -0300

    primeira etapa da criacao da tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { SafeAreaView } from "react-native";
 import Login from "./modules/login";
 
 const App = () => {
-  <SafeAreaView>
-    <Login />
-  </SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
+  );
 };
 
 export default App;
