23442617c58f3e0d128b01b2ba014e6f7d955286
Felipe Oliveira <felipe.oliveira@rumosolucoes.com>
2023-09-21 11:40:23 -0300

    primeiros etepa de criacao na tela de login

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import { AppRegistry } from "react-native";
 
 import { name as appName } from "./app.json";
-import StyleAPP from "./src/Style";
+import App from "./src/App";
 
-AppRegistry.registerComponent(appName, () => StyleAPP);
+AppRegistry.registerComponent(appName, () => App);
